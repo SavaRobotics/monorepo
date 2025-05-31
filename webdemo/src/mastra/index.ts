7b339d7a60506193db2413f1069a78e44bba4776
Sava-VedicP <vedic@savarobotics.com>
2025-05-31 16:22:58 -0700

    mastra working with example

---
+++ webdemo/src/mastra/index.ts
@@ -0,0 +1,17 @@
+import { Mastra } from '@mastra/core/mastra';
+import { PinoLogger } from '@mastra/loggers';
+import { weatherWorkflow } from './workflows';
+import { weatherAgent } from './agents';
+import { LibSQLStore } from "@mastra/libsql";
+
+export const mastra = new Mastra({
+  workflows: { weatherWorkflow },
+  agents: { weatherAgent },
+  logger: new PinoLogger({
+    name: 'Mastra',
+    level: 'info',
+  }),
+  storage: new LibSQLStore({
+    url: "file:./mastra.db",
+  }),
+});
